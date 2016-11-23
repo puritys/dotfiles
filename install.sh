108d7533997c469293fa0fdf12f5667f32571138
puritys <puritys.chen@outlook.com>
2016-11-23 17:39:43 +0800

    Modify script for installing linux setting

--- install.sh
+++ install.sh
@@ -1,6 +1,24 @@
 touch ~/.bash_host
 cp basic/.* ~/
 
+while true; do
+    if [ "x" == "x" ];then
+        break;
+    fi  
+    case "" in
+      -p | --vimPlugin   ) installVimPlugin=1; shift 1 ;;
+      -d | --debug ) DEBUG=true; shift 1 ;;
+      -h | --help  ) 
+          echo "Usage:"
+          echo "-p: install vim plugin"
+          shift 1 
+          ;;  
+      --) echo "-- is not a correct option.";shift 1; ;;
+      * ) echo " is not a correct option.";shift 1; ;;
+    esac
+done
+
+installVimPlugin=$1
 # .vim style
 if [[ `uname` == 'Darwin' ]]; then
     cp -R vim/ ~/.vim
@@ -19,9 +37,17 @@ if [ ! -d ~/.vim/bundle/Vundle.vim ]; then
 fi
 
 
+if [ "x$installVimPlugin" != "x" ];then
+    sudo rm -rf ~/.vim/bundle/vim-snipmate/
+    vim -c :PluginInstall -c :q
+fi
+
 if [ ! -d ~/.vim/bundle/vim-snipmate/snippets ]; then
     mkdir -p ~/.vim/bundle/vim-snipmate/snippets
 fi
-sudo rm ~/.vim/bundle/vim-snipmate/snippets/*
+
+sudo rm -f ~/.vim/bundle/vim-snipmate/snippets/*
+
+
 cp vim-snipmate/*.snippets ~/.vim/bundle/vim-snipmate/snippets/
 
