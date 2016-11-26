add107207ce44d7b76f90666a837339bffc510c6
puritys <puritys.chen@gmail.com>
2016-11-26 22:55:19 +0800

    Improve script

--- install.sh
+++ install.sh
@@ -1,6 +1,12 @@
 touch ~/.bash_host
 cp basic/.* ~/
 
+if hash sudo 2>/dev/null; then
+    sudo="sudo"
+else
+    sudo=""
+fi
+
 while true; do
     if [ "x" == "x" ];then
         break;
@@ -28,17 +34,13 @@ fi
 
 
 
-# ssh
-#mkdir ~/.ssh
-#cp ssh/* ~/.ssh/
-#chmod 400 ~/.ssh/config
 if [ ! -d ~/.vim/bundle/Vundle.vim ]; then
     git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 fi
 
 
 if [ "x$installVimPlugin" != "x" ];then
-    sudo rm -rf ~/.vim/bundle/vim-snipmate/
+    $sudo rm -rf ~/.vim/bundle/vim-snipmate/
     vim -c :PluginInstall -c :q
 fi
 
@@ -46,7 +48,7 @@ if [ ! -d ~/.vim/bundle/vim-snipmate/snippets ]; then
     mkdir -p ~/.vim/bundle/vim-snipmate/snippets
 fi
 
-sudo rm -f ~/.vim/bundle/vim-snipmate/snippets/*
+$sudo rm -f ~/.vim/bundle/vim-snipmate/snippets/*
 
 
 cp vim-snipmate/*.snippets ~/.vim/bundle/vim-snipmate/snippets/
