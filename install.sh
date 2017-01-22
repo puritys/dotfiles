1e9d07c8597493d292ce2f8cc65aa29400eae0b1
puritys <puritys.chen@gmail.com>
2017-01-22 17:21:41 +0800

    Add docker alias and fix options of shell script

--- install.sh
+++ install.sh
@@ -1,4 +1,25 @@
 #!/bin/bash
+
+while true; do
+    if [ "x$1" == "x" ];then
+        break;
+    fi  
+    case "$1" in
+      -p | --vimPlugin   ) installVimPlugin=1; shift 1 ;;
+      -d | --debug ) DEBUG=true; shift 1 ;;
+      -k | --docker) DOCKER=true; shift 1;;
+      -h | --help  ) 
+          echo "Usage:"
+          echo "-p: install vim plugin"
+          echo "-p: install docker alias"
+          shift 1
+          exit 1 
+          ;;  
+      --) echo "-- is not a correct option.";shift 1; ;;
+      * ) echo "$1 is not a correct option.";shift 1; ;;
+    esac
+done
+
 touch ~/.bash_host
 cp basic/.* ~/
 
@@ -8,23 +29,6 @@ else
     sudo=""
 fi
 
-while true; do
-    if [ "x" == "x" ];then
-        break;
-    fi  
-    case "" in
-      -p | --vimPlugin   ) installVimPlugin=1; shift 1 ;;
-      -d | --debug ) DEBUG=true; shift 1 ;;
-      -h | --help  ) 
-          echo "Usage:"
-          echo "-p: install vim plugin"
-          shift 1 
-          ;;  
-      --) echo "-- is not a correct option.";shift 1; ;;
-      * ) echo " is not a correct option.";shift 1; ;;
-    esac
-done
-
 # .vim style
 if [[ `uname` == 'Darwin' ]]; then
     cp -R vim/ ~/.vim
@@ -32,12 +36,14 @@ else
     cp -rT vim ~/.vim
 fi
 
-
-
 if [ ! -d ~/.vim/bundle/Vundle.vim ]; then
     git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 fi
-
+echo $DOCKER
+if [ "x$DOCKER" == "x" ];then 
+    echo "Without docker"
+    rm -f ~/.alias_docker
+fi
 
 if [ "x$installVimPlugin" != "x" ];then
     $sudo rm -rf ~/.vim/bundle/vim-snipmate/
