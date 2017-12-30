9e798bf753f7fe018901134845e42d6300f58515
puritys <puritys.chen@gmail.com>
2017-12-30 16:02:26 +0000

    Add script

---
+++ bin/exec.sh
@@ -0,0 +1,108 @@
+while true; do
+    if [ "x$1" == "x" ];then
+        break;
+    fi
+    case "$1" in
+      -p | --process   ) process=$2; shift 2 ;;
+      -c | --command   ) command=$2; shift 2 ;;
+      -f | --file   ) file=$2; shift 2 ;;
+      -d | --debug ) DEBUG=true; shift 1 ;;
+      -h | --help  ) 
+          echo "Usage:"
+          echo "-p: process, lib, iptables, os"
+          echo "-c: command."
+          echo "-f: file."
+          echo "-d: Enable debug"
+          shift 1 
+          exit 0
+          ;;
+      --) echo "-- is not a correct option.";shift 1; ;;
+      * ) echo "$1 is not a correct option.";shift 1; ;;
+    esac
+done
+# --------
+# common lib
+# --------
+
+lib_help() {
+    echo "lib usage:"
+    echo "-p lib -c get_date: To return today's date, variable = date"
+    echo "-p lib -c zip_file -f file.txt: to gzip file and change the file name"
+
+}
+
+lib_get_date() {
+    date=`date +%Y-%m-%d`
+    echo $date
+}
+
+lib_gzip_file() {
+    lib_get_date
+    action=""
+    if [ "x" != "x$1" ]; then
+        file=$1
+    fi
+    if [ "x" != "x$2" ]; then
+        action=$2
+    fi
+    if [ "x" == "x$file" ];then
+        echo "file is empty"
+        return;
+    fi
+    if [ ! -f $file ]; then
+        echo "File $file not found."
+    fi
+    echo "zip file: $file to $file-$date.gz"
+    gzip $file
+    mv $file.gz $file-$date.gz
+    if [ "xsetEmpty" == "x$action" ]; then
+        touch $file
+    fi
+}
+
+# --------
+# OS Basic command
+# --------
+
+os_rotate_log() {
+    lib_gzip_file /var/log/cron setEmpty
+    lib_gzip_file /var/log/messages setEmpty
+}
+
+os_clean_log() {
+    rm -f /var/log/cron*.gz
+    rm -f /var/log/messages*.gz
+}
+
+# --------
+# iptables
+# --------
+iptables_help() {
+    echo "iptables usage:"
+    echo "-p iptables -c restore: To restore setting"
+    echo "-p iptables -c save: To save setting"
+}
+
+iptables_restore() {
+
+    if [ ! -f iptableBackup.rule ]; then
+        echo "File iptableBackup.rule not found."
+    else
+        sudo iptables-restore < iptableBackup.rule
+    fi
+}
+
+iptables_save() {
+    sudo iptables-save > iptableBackup.rule
+}
+
+
+if [ "x" != "x$process" ] && [ "x" == "x$command" ]; then
+    $process"_help"
+fi
+
+if [ "x" != "x$process" ] && [ "x" != "x$command" ]; then
+    $process"_"$command
+fi 
+
+
