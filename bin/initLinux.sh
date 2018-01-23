3868ad57ab599da0897adc5b6dd7b0eda7878716
puritys <puritys.chen@gmail.com>
2018-01-23 17:48:09 +0000

    Add script to initial linux env

--- bin/initLinux.sh
+++ bin/initLinux.sh
@@ -1,1 +1,10 @@
 sudo yum install screen vim git wget
+
+acount=`whoami`
+# install node
+mkdir -p ~/bin
+wget https://nodejs.org/dist/v8.9.4/node-v8.9.4-linux-x64.tar.xz ~/bin
+cd ~/bin
+tar -xvf node-v8.9.4-linux-x64.tar.xz
+sudo ln -sf /home/$account/bin/node-v8.9.4-linux-x64/bin/node /usr/bin/node
+sudo ln -sf /home/$account/bin/node-v8.9.4-linux-x64/bin/npm /usr/bin/npm
