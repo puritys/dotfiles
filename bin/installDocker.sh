73ce77c37517ac894741a75f60058be7684920f1
puritys <puritys.chen@gmail.com>
2018-06-27 08:28:32 +0000

    add script to install docker compose

--- bin/installDocker.sh
+++ bin/installDocker.sh
@@ -19,3 +19,5 @@ sudo chown `whoami`  /var/run/docker.sock
 
 # install docker compose
 sudo curl --tlsv1.2 -L https://github.com/docker/compose/releases/download/1.21.2/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+
