f5becbaa8449d2521d859f3c2793690ecd474156
puritys <puritys.chen@gmail.com>
2018-09-20 02:31:35 +0000

    install docker for rhel

--- bin/installDocker.sh
+++ bin/installDocker.sh
@@ -1,23 +1,32 @@
-# RHEL
-# sudo yum -y install docker docker-registry
 
+os=`cat /etc/*-release | grep -o "^ID=\"[a-z]*\""`
+echo "os is $os"
 
-# centos
-sudo yum install -y yum-utils \
-  device-mapper-persistent-data \
-  lvm2
+if [[ $os == *"centos"* ]]; then
+    # centos
+    sudo yum install -y yum-utils \
+      device-mapper-persistent-data \
+      lvm2
 
-sudo yum-config-manager \
-    --add-repo \
-    https://download.docker.com/linux/centos/docker-ce.repo
+    sudo yum-config-manager \
+        --add-repo \
+        https://download.docker.com/linux/centos/docker-ce.repo
 
-sudo yum-config-manager --enable docker-ce-edge
-sudo yum install docker-ce
+    sudo yum-config-manager --enable docker-ce-edge
+    sudo yum install docker-ce
 
-sudo service docker start
-sudo chown `whoami`  /var/run/docker.sock
+    sudo service docker start
+    sudo chown `whoami`  /var/run/docker.sock
 
-# install docker compose
-sudo curl --tlsv1.2 -L https://github.com/docker/compose/releases/download/1.21.2/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
+    # install docker compose
+    sudo curl --tlsv1.2 -L https://github.com/docker/compose/releases/download/1.21.2/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
+    sudo chmod +x /usr/local/bin/docker-compose
+
+elif [[ $str == *"rhel"* ]]; then
+    # RHEL
+    sudo yum -y install docker docker-registry
+    sudo service docker start
+    sudo chown `whoami`  /var/run/docker.sock
+
+fi
 
