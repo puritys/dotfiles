81f1cd696e73debe0907c58556591dfc882800cc
puritys <puritys.chen@gmail.com>
2019-05-24 08:21:38 +0000

    update

--- bin/appendJavaClasspath.sh
+++ bin/appendJavaClasspath.sh
@@ -1,6 +1,22 @@
 #!/bin/sh
 
 file=.classpath
+while true; do
+    if [ "x$1" == "x" ];then
+        break;
+    fi
+    case "$1" in
+      -f | --file   ) file=$2; shift 2 ;;
+      -h | --help  ) 
+          echo "Usage:"
+          echo "-f: file -f .classpaht."
+          shift 1 
+          exit 0
+          ;;
+      --) echo "-- is not a correct option.";shift 1; ;;
+      * ) echo "$1 is not a correct option.";shift 1; ;;
+    esac
+done
 appendPaths="target/generated-sources/java,target/generated-sources/annotations"
 
 
