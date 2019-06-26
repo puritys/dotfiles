cebf516382cc6c2c6900faaa652f5bb87bfa716c
puritys <puritys.chen@gmail.com>
2019-06-26 05:19:34 +0000

    update

--- scripts/installCommonCommand.sh
+++ scripts/installCommonCommand.sh
@@ -5,6 +5,7 @@ fi
 
 if [ ! -z $INSTALL_NODE ] &&  [ true == $INSTALL_NODE ];then
     if [ ! -d node ]; then
+        sudo mkdir -p /usr/local/lib/node_modules
         if [ -f /usr/local/bin/npm ]; then
             sudo rm -f /usr/local/bin/npm
         fi
@@ -20,7 +21,7 @@ if [ ! -z $INSTALL_NODE ] &&  [ true == $INSTALL_NODE ];then
         wget https://nodejs.org/dist/v10.16.0/node-v10.16.0-linux-x64.tar.xz
         tar -xf node-v10.16.0-linux-x64.tar.xz 
         mv node-v10.16.0-linux-x64 node
-        sudo cp -r node/lib/node_modules/npm/ /usr/local/lib/node_modules/
+        sudo cp -r node/lib/node_modules/npm/ /usr/local/lib/node_modules/npm
         sudo cp node/bin/npm  /usr/local/bin/
         sudo cp node/bin/node  /usr/local/bin/
         sudo ln -sf  /usr/local/lib/node_modules/npm/bin/npm-cli.js  /usr/local/bin/npm
