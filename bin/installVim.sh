991999e1aa3409b9701cb663d549772887171f95
puritys <puritys.chen@gmail.com>
2021-09-09 03:10:59 -0400

    upgrade vim version

--- bin/installVim.sh
+++ bin/installVim.sh
@@ -1,7 +1,13 @@
 sudo yum install  -y ncurses-devel
-wget https://github.com/vim/vim/archive/v7.4.2367.tar.gz
-tar -zxvf v7.4.2367.tar.gz 
-cd vim-7.4.2367
-./configure --enable-multibyte --enable-pythoninterp=yes --prefix=/usr --with-x --x-includes=/usr/include/X11/ --x-libraries=/usr/lib64/
+if [ ! -f vim.tar.gz ]; then
+    wget https://github.com/vim/vim/archive/v8.2.3401.tar.gz
+    mv v8.2.3401.tar.gz vim.tar.gz
+fi
+tar -zxvf vim.tar.gz 
+mv `ls -D |grep 'vim-[0-9]' | sed 's/\///'` vim_src
+cd vim_src
+./configure ./configure --prefix=/usr  --enable-multibyte \
+         --enable-pythoninterp=yes --with-python2-config-dir=/usr/bin/ \
+        --enable-python3interp=yes --with-python3-config-dir=/usr/bin/
 gmake
 sudo gmake install
