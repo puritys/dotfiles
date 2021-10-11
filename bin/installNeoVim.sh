b0d0d4b0d64bbea5167435d42fc208e79645d233
puritys <puritys.chen@gmail.com>
2021-10-12 06:04:53 +0800

    update for neovim and coc

--- bin/installNeoVim.sh
+++ bin/installNeoVim.sh
@@ -1,4 +1,12 @@
 if [[ `uname` == 'Darwin' ]]; then
     brew install neovim
     python3 -m pip install --user --upgrade pynvim
+    #pip3 install pynvim
+else
+    # yum -y install libtool
+    wget https://github.com/neovim/neovim/releases/download/v0.5.1/nvim-linux64.tar.gz
+    tar -zxvf nvim-linux64.tar.gz
+    cd nvim-linux64
+    make CMAKE_BUILD_TYPE=Release
+    make install
 fi
