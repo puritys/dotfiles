56bc4a3cfafc6df6318afc820a4ce377f3a4e21f
puritys <puritys.chen@gmail.com>
2022-04-18 18:22:49 +0000

    udpate

--- bin/installNeoVim.sh
+++ bin/installNeoVim.sh
@@ -7,6 +7,7 @@ else
     wget https://github.com/neovim/neovim/releases/download/v0.5.1/nvim-linux64.tar.gz
     tar -zxvf nvim-linux64.tar.gz
     sudo cp -r nvim-linux64/share/nvim/ /usr/share/
-    sudo cp -r nvim-linux64/bin/nvim/ /usr/bin/
-
+    sudo cp -r nvim-linux64/bin/nvim /usr/bin/
+    sudo chmod -R 755 /usr/share/nvim/
+    sudo chmod 755 /usr/bin/nvim
 fi
