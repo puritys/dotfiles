48e275033a160388624345b09140287b8edf2707
puritys <puritys.chen@gmail.com>
2023-02-01 05:38:00 +0000

    install gradle

---
+++ bin/installGradle.sh
@@ -0,0 +1,7 @@
+rm -rf gradle_tmp
+mkdir gradle_tmp
+cd gradle_tmp
+wget https://services.gradle.org/distributions/gradle-6.9.3-bin.zip
+unzip gradle-6.9.3-bin.zip
+mv gradle-6.9.3 ~/gradle
+rm -rf gradle_tmp
