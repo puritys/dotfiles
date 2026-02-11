2f08e3cbbb3c5064f026c66f7e81e5c59aa47b9a
puritys <puritys.chen@gmail.com>
2026-02-11 01:51:02 +0000

    update

--- bin/installNeoVim.sh
+++ bin/installNeoVim.sh
@@ -1,16 +1,18 @@
 if [[ `uname` == 'Darwin' ]]; then
-    wget https://github.com/neovim/neovim/releases/download/v0.6.1/nvim-macos.tar.gz
+    wget https://github.com/neovim/neovim/releases/download/v0.8.1/nvim-macos.tar.gz
     tar -zxvf nvim-macos.tar.gz
-    sudo mkdir -p /opt/homebrew/Cellar/neovim/0.6.1/
-    sudo cp -r nvim-osx64/* /opt/homebrew/Cellar/neovim/0.6.1/
-    sudo ln -sf /opt/homebrew/Cellar/neovim/0.6.1/bin/nvim  /usr/local/bin/vim
+    sudo mkdir -p /opt/homebrew/Cellar/neovim/0.8.1/
+    sudo cp -r nvim-osx64/* /opt/homebrew/Cellar/neovim/0.8.1/
+    sudo ln -sf /opt/homebrew/Cellar/neovim/0.8.1/bin/nvim  /usr/local/bin/vim
     rm -rf nvim-osx64 nvim-macos.tar.gz
     #brew install neovim
     #python3 -m pip install --user --upgrade pynvim
 else
     # yum -y install libtool
-    wget https://github.com/neovim/neovim/releases/download/v0.6.1/nvim-linux64.tar.gz
+    sudo rm -rf nvim-linux*
+    wget https://github.com/neovim/neovim-releases/releases/download/v0.10.0/nvim-linux64.tar.gz
     tar -zxvf nvim-linux64.tar.gz
+    sudo rm -f /usr/bin/nvim
     sudo cp -r nvim-linux64/share/nvim/ /usr/share/
     sudo cp -r nvim-linux64/bin/nvim /usr/bin/
     sudo chmod -R 755 /usr/share/nvim/
